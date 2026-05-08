0996a937e11c514aeef381682ee7ddf57d38a752
Harshtaker <harshshukla0303@gmail.com>
2026-05-08 13:48:51 +0530

    csv generator removed

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,5 @@ import streamlit as st
 @st.cache_data
 def load_data():
     df = pd.read_csv("products.csv")
-    # Clean the tags just in case
-    df['tags'] = df['tags'].fillna('')
+    df['tags'] = df['tags'].fillna('')      #to clean tags in case 
     return df
--- engine.py
+++ engine.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def get_recommendations(target_product_name, df):
-    # Create a "Content Soup" for better matching
+    
     df['content'] = df['category'] + " " + df['description'] + " " + df['tags']
     
     tfidf = TfidfVectorizer(stop_words='english')
@@ -16,8 +16,7 @@ def get_recommendations(target_product_name, df):
         sim_scores = list(enumerate(cosine_sim[idx]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
         
-        # Get top 4 similar products (skipping the item itself)
-        top_indices = [i[0] for i in sim_scores[1:5]]
+        top_indices = [i[0] for i in sim_scores[1:5]]           
         return df.iloc[top_indices]
     except:
         return pd.DataFrame()
